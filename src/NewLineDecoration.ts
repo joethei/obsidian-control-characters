9c610b6fac77a81ff2b0d149d1fea5f8210dde31
Johannes Theiner <kontakt@joethei.xyz>
2022-03-28 16:24:32 +0200

    fully working

--- src/NewLineDecoration.ts
+++ src/NewLineDecoration.ts
@@ -5,6 +5,10 @@ import {SymbolWidget} from "./SymbolWidget";
 import ControlCharacterPlugin from "./main";
 import {statefulDecorations} from "./StatefulDecoration";
 
+/*
+using a custom decoration here because a MatchDecoration only seems to allow replacing decorations, not widget ones.
+ */
+
 interface TokenSpec {
 	from: number;
 	to: number;
@@ -12,7 +16,7 @@ interface TokenSpec {
 
 class StatefulDecorationSet {
 	editor: EditorView;
-	decoCache: { [cls: string]: Decoration } = Object.create(null);
+	decoCache: Decoration;
 
 	constructor(editor: EditorView) {
 		this.editor = editor;
@@ -21,16 +25,16 @@ class StatefulDecorationSet {
 	async computeAsyncDecorations(tokens: TokenSpec[]): Promise<DecorationSet | null> {
 		const decorations: Range<Decoration>[] = [];
 		for (const token of tokens) {
-			let deco = this.decoCache[""];
+			let deco = this.decoCache;
 			if (!deco) {
-				deco = this.decoCache[""] = Decoration.widget({ widget: new SymbolWidget("↵") });
+				deco = this.decoCache = Decoration.widget({ widget: new SymbolWidget("↵") });
 			}
 			decorations.push(deco.range(token.from, token.from));
 		}
 		return Decoration.set(decorations, true);
 	}
 
-	debouncedUpdate = debounce(this.updateAsyncDecorations, 100, true);
+	debouncedUpdate = debounce(this.updateAsyncDecorations, 10, true);
 
 	async updateAsyncDecorations(tokens: TokenSpec[]): Promise<void> {
 		const decorations = await this.computeAsyncDecorations(tokens);
@@ -64,12 +68,14 @@ function buildViewPlugin(plugin: ControlCharacterPlugin) {
 				if(!plugin.settings.newLine) {
 					return;
 				}
-
 				const targetElements: TokenSpec[] = [];
+
 				for (const {from, to} of view.visibleRanges) {
+					console.log("visible range " + from + " " + to);
 					const text = view.state.sliceDoc(from, to);
-					for (const regExpMatchArray of text.matchAll(/\n$/gm)) {
-						targetElements.push({from: regExpMatchArray.index, to: regExpMatchArray.index});
+					for (const match of text.matchAll(/\n$|[\s\S]$/gm)) {
+						const index = from + match.index + 1;
+						targetElements.push({from: index, to: index});
 					}
 				}
 				this.decoManager.debouncedUpdate(targetElements);
